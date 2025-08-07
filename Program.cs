e3cd3d74ecf0db48be2c9e92383f052c09f2f911
tatojastine-n <jajanicoletato@gmail.com>
2025-08-07 11:13:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Character_Access
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[3];
+
+            Console.WriteLine("Enter 3 names:");
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.Write($"Name {i + 1}: ");
+                names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nResults:");
+
+            foreach (string name in names)
+            {
+                if (!string.IsNullOrEmpty(name)) // Check if the name is not empty
+                {
+                    char firstChar = name[0]; // Get the first character
+                    char lastChar = name[name.Length - 1]; // Get the last character
+                    string upperFirstChar = char.ToUpper(firstChar).ToString(); // Uppercase first character
+                    string upperLastChar = char.ToUpper(lastChar).ToString(); // Uppercase last character
+
+                    // Print the results
+                    Console.WriteLine($"Original Name: {name}");
+                    Console.WriteLine($"First Character: {firstChar} (Uppercase: {upperFirstChar})");
+                    Console.WriteLine($"Last Character: {lastChar} (Uppercase: {upperLastChar})");
+                    Console.WriteLine(); // Blank line for better readability
+                }
+                else
+                {
+                    Console.WriteLine("Name cannot be empty.");
+                }
+
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("String Character Access")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("String Character Access")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("56e9564a-68e9-4a16-9d07-3521fbdfea20")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
